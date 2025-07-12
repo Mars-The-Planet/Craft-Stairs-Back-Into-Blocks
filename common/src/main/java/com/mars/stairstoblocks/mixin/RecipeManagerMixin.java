9b8d1a7e57b77a1d88f1a9bcfb0c3d6e808810e0
Mars <ruzicka.maty@gmail.com>
2025-07-12 18:22:37 +0200

    removed mixins local

--- common/src/main/java/com/mars/stairstoblocks/mixin/RecipeManagerMixin.java
+++ common/src/main/java/com/mars/stairstoblocks/mixin/RecipeManagerMixin.java
@@ -2,22 +2,18 @@ package com.mars.stairstoblocks.mixin;
 
 import com.google.common.collect.Lists;
 import com.google.gson.*;
-import com.llamalad7.mixinextras.sugar.Local;
-import com.llamalad7.mixinextras.sugar.ref.LocalRef;
 import com.mars.deimos.datagen.DeimosRecipeGenerator;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.server.packs.resources.Resource;
 import net.minecraft.server.packs.resources.ResourceManager;
 import net.minecraft.util.profiling.ProfilerFiller;
-import net.minecraft.world.item.crafting.RecipeHolder;
 import net.minecraft.world.item.crafting.RecipeManager;
 import net.minecraft.world.item.crafting.RecipeMap;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import java.io.IOException;
