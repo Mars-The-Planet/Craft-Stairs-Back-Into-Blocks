be4e5997e02a36021e7767f808ef3aa26379474b
Mars <ruzicka.maty@gmail.com>
2025-07-13 00:02:35 +0200

    backported to 1.20.4

---
+++ common/src/main/java/com/mars/stairstoblocks/CommonClass.java
@@ -0,0 +1,33 @@
+package com.mars.stairstoblocks;
+
+import com.google.common.collect.Lists;
+import com.mars.deimos.config.DeimosConfig;
+import com.mars.deimos.datagen.DeimosRecipeGenerator;
+import net.minecraft.resources.ResourceLocation;
+
+import static com.mars.stairstoblocks.StairsToBlocksConfig.*;
+
+public class CommonClass {
+    public static void init() {
+        DeimosConfig.init(Constants.MOD_ID, StairsToBlocksConfig.class);
+
+        if(add_recipes_manually){
+            for (int i = 0; i < block_stairs_list.size(); i++) {
+                String current = block_stairs_list.get(i);
+                String[] set = ((current).replaceAll("\\s","")).split(",");
+
+                DeimosRecipeGenerator.createShapedRecipeJson(
+                        Lists.newArrayList(
+                                '#'
+                        ),
+                        Lists.newArrayList(new ResourceLocation(set[1])),
+                        Lists.newArrayList("item"),
+                        Lists.newArrayList(
+                                "##",
+                                "##"
+                        ),
+                        new ResourceLocation(set[0]), block_amount);
+            }
+        }
+    }
+}
---
+++ common/src/main/java/com/mars/stairstoblocks/platform/Services.java
@@ -0,0 +1,15 @@
+package com.mars.stairstoblocks.platform;
+
+import com.mars.stairstoblocks.Constants;
+import com.mars.stairstoblocks.platform.services.IPlatformHelper;
+
+import java.util.ServiceLoader;
+
+public class Services {
+    public static final IPlatformHelper PLATFORM = load(IPlatformHelper.class);
+
+    public static <T> T load(Class<T> clazz) {
+        final T loadedService = ServiceLoader.load(clazz).findFirst().orElseThrow(() -> new NullPointerException("Failed to load service for " + clazz.getName()));
+        return loadedService;
+    }
+}
